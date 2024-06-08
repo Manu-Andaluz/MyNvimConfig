5548c98c95bd15454ff5ef649c20ec0906ec6368
Manu-Andaluz <manupeandaluz@gmail.com>
2024-06-07 23:10:29 -0300

    new changes

---
+++ lua/config/autocmds.lua
@@ -0,0 +1,21 @@
+-- Autocmds are automatically loaded on the VeryLazy event
+-- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
+vim.api.nvim_create_autocmd("BufEnter", {
+  pattern = "*",
+  command = "set shiftwidth=4",
+})
+
+
+vim.api.nvim_create_autocmd("InsertLeave", {
+  pattern = "*",
+  command = "set nopaste",
+})
+
+-- Fix canceallevel for json files
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "json", "jsonc" },
+  callback = function()
+    vim.wo.spell = false
+    vim.wo.conceallevel = 0
+  end,
+})
