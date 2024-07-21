886b02eb433eaf50110d00cb2fa57448e95265e9
Manuel Andaluz <manupeandaluz@gmail.com>
2024-07-20 22:21:31 -0300

    updating my config

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,21 +1,29 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
-vim.api.nvim_create_autocmd("BufEnter", {
-  pattern = "*",
-  command = "set shiftwidth=4",
-})
 
+-- Set default indentation to 2 spaces
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = "*",
+	callback = function()
+		vim.bo.shiftwidth = 2
+		vim.bo.tabstop = 2
+		vim.bo.softtabstop = 2
+		vim.bo.expandtab = true
+		vim.bo.autoindent = true
+		vim.bo.smartindent = true
+	end,
+})
 
 vim.api.nvim_create_autocmd("InsertLeave", {
-  pattern = "*",
-  command = "set nopaste",
+	pattern = "*",
+	command = "set nopaste",
 })
 
--- Fix canceallevel for json files
+-- Fix conceallevel for json files
 vim.api.nvim_create_autocmd("FileType", {
-  pattern = { "json", "jsonc" },
-  callback = function()
-    vim.wo.spell = false
-    vim.wo.conceallevel = 0
-  end,
+	pattern = { "json", "jsonc" },
+	callback = function()
+		vim.wo.spell = false
+		vim.wo.conceallevel = 0
+	end,
 })
